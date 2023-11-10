506167d5ba150af38091df51ab4f879269deee9c
Seviko Attalarik <attalarik.handoko@gmail.com>
2023-11-10 21:44:17 +0700

    initial commit

---
+++ controller/user.js
@@ -0,0 +1,136 @@
+const Division = require('../model/Division');
+const User = require('../model/User');
+
+const getAllUser = async (req, res, next)=>{
+  try {
+    const users = await User.findAll({
+      include: {
+        model: Division,
+        as: 'division',
+        required: true,
+        attributes: ['id', 'name']
+      },
+      attributes: ['id', 'fullname', 'angkatan']
+    });
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully get all user",
+      users: users,
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+const getUserById = async (req,res,next)=>{
+  try {
+    //TUGAS NOMOR 2 cari user berdasarkan userId
+    const {userId} = req.params
+    const users = await User.findOne({
+      where: {
+        id: userId
+      },
+      include: {
+        model: Division,
+        as: 'division',
+        attributes: ['id', 'name']
+      },
+      attributes: ['id', 'fullname', 'angkatan']
+    })
+    if (users == undefined){
+      res.status(400).json({
+        status: "Error",
+        message: `User with id ${userId} is not existed`
+      })
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully get user by id",
+      users: users
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+const postUser = async(req,res,next)=>{
+  try {
+    const {
+      fullName, nim, angkatan, email, password, division
+    } = req.body
+
+    //cari divisi id
+    //pakai await untuk menghindari penulisan then
+    const user_division = await Division.findOne({
+      where:{
+        name: division
+      }
+    });
+
+    //SELECT * FROM DIVISION WHERE name = division
+    if(user_division == undefined){
+      res.status(400).json({
+        status: "Error",
+        message: `${division} is not existed`
+      })
+    }
+
+    //insert data ke tabel User
+    const currentUser = await User.create({
+      //nama field: data
+      fullName: fullName,
+      //jika nama field == data maka bisa diringkas
+      email,
+      password,
+      angkatan,
+      nim,
+      divisionId: user_division.id
+    })
+
+    //send response
+    res.status(201).json({
+      status: "success",
+      message: "Successfuly create User",
+      user: {
+        fullName: currentUser.fullName,
+        division: currentUser.division
+      }
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const deleteUser = (req,res,next)=>{
+  try {
+    const {userId} = req.params;
+
+    //mencari index user dari array model user
+    const targetedIndex = User.findIndex((element)=>{
+      return element.id == userId
+    })
+
+    //user tidak ketemu
+    if(targetedIndex === -1){
+      res.status(400).json({
+        status: "Error",
+        message: `User with id ${userId} is not existed`
+      })
+    }
+
+    //hapus array pada [targetedIndex] sebanyak 1 buah element
+    User.splice(targetedIndex, 1);
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully delete user"
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  getAllUser, getUserById, postUser, deleteUser
+}
